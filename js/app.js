a0a139e487de6892fbdfc0756df936e81ed32ce6
PaulineBr0d <briaud.pauline@gmail.com>
2025-08-29 12:01:21 +0200

    icon map button

--- js/app.js
+++ js/app.js
@@ -201,10 +201,10 @@ function loadDetail() {
           </div> 
           <div class="right-content">
             <div class="map">
-              <button id="first"><i class="fas fa-backward-fast"></i></button>
+              <button id="first"><i class="fas fa-backward-step"></i></button>
               <button id="prev"><i class="fas fa-chevron-left"></i></button>
               <button id="next"><i class="fas fa-chevron-right"></i></button>
-              <button id="last"><i class="fas fa-forward-fast"></i></button>
+              <button id="last"><i class="fas fa-forward-step"></i></button>
             </div>
             <div class="center">
               <div class="wrapper">
